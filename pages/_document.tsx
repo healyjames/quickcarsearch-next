9e175d776e7c65c218194425dc10f63ca199c85b
James <healyjames97@gmail.com>
2023-10-22 22:21:54 +0100

    chore: add google ads script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -26,6 +26,7 @@ export default function Document() {
             <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@400;700&display=swap" rel="stylesheet" />
             <link rel="icon" type="image/png" href="/assets/logo/logo-icon-circle-orange.svg"/>
             <Script src="https://kit.fontawesome.com/75a401b8c1.js" crossOrigin="anonymous" />
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2248724540494118" crossOrigin="anonymous"></script>
         </Head>
         <body
           style={{
