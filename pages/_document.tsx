aaecda59f83c372f84b286f83713e126af5d5627
James <healyjames97@gmail.com>
2023-10-23 22:35:59 +0100

    fix: package and lint errors

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,10 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+import React from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+import { DocumentContext } from 'next/document'
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
