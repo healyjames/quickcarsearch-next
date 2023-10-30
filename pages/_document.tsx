672bbf1bd126c3a5dd1f1b51b362193f0be9f4c0
James <healyjames97@gmail.com>
2023-10-30 11:45:46 +0000

    fix: google ads script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 import { DocumentContext } from 'next/document'
+import Script from 'next/script';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -68,6 +69,7 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <Script strategy="beforeInteractive" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2248724540494118" crossOrigin="anonymous" />
         </body>
       </Html>
     )
