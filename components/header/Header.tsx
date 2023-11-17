0ae55bf56d9c7423e598e3603ad0c20981e726c9
James <healyjames97@gmail.com>
2023-11-17 23:12:32 +0000

    chore: add sidebar to new header

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link';
 import styled from 'styled-components'
 
+import { Sidebar } from '../sidebar/Sidebar';
+import { Overlay, NavOpenButton } from './styles'
 
 const HeaderContainerInner = styled.div`
 	display: flex;
@@ -11,26 +13,53 @@ const HeaderContainerInner = styled.div`
 	padding: 0 ${props => props.theme.core.padding}rem;
 
 	@media (min-width: ${props => props.theme.breakpoints.lg}px) {
-        justify-content: flex-end;
+        justify-content: space-between;
     }
 `
 
 const AcronymContainer = styled.div`
+	display: none;
 	will-change: transform;
   	transform: scale(0.85);
 	margin-left: 1rem;
+
+	@media (min-width: ${props => props.theme.breakpoints.lg}px) {
+        display: block;
+    }
 `;
 
 const HomeContainer = styled.div`
 	background-color: ${(props) => props.theme.colors.brand};
-	padding: ${props => (props.theme.core.padding * 2).toFixed(2)}rem 0;
+	padding: ${props => (props.theme.core.padding * 1.5).toFixed(2)}rem 0;
+`
+
+const LogoContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	flex-wrap: none;
+	gap: ${props => props.theme.core.grid.gap}rem;
+	align-items: center;
 `
 
 export const Header = () => {
+	const [sidebarOpen, setSidebarOpen] = useState(false)
+
+	const toggleSidebar = () => {
+		setSidebarOpen(!sidebarOpen);
+	}
+	
 	return(
 		<HomeContainer>
-			<div>
-				<HeaderContainerInner>
+			<HeaderContainerInner>
+			<NavOpenButton onClick={toggleSidebar}>
+				<Image
+					src={"/assets/icons/bars-solid.svg"}
+					alt="Navigation open icon"
+					width={26}
+					height={26}
+				/>
+			</NavOpenButton>
+				<LogoContainer>
 					<Link href="/" tabIndex={-1}>
 						<Image
 							src={"/assets/logo/logo-icon-white.svg"}
@@ -41,16 +70,20 @@ export const Header = () => {
 						/>
 					</Link>
 					<AcronymContainer>
-						<Image
-							src={"/assets/logo/logo-acronym-white.svg"}
-							alt="Picture of the author"
-							width="60"
-							height="50"
-							style={{width: '75px', height: 'auto'}}
-						/>
+						<Link href="/" tabIndex={-1}>
+							<Image
+								src={"/assets/logo/logo-acronym-white.svg"}
+								alt="Picture of the author"
+								width="60"
+								height="50"
+								style={{width: '75px', height: 'auto'}}
+							/>
+						</Link>
 					</AcronymContainer>
-				</HeaderContainerInner>
-			</div>
+				</LogoContainer>
+			</HeaderContainerInner>
+			<Overlay show={sidebarOpen} onClick={toggleSidebar} />
+			<Sidebar open={sidebarOpen} close={toggleSidebar} />
 		</HomeContainer>
 	)
 }
