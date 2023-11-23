43e0cf568655289b9a7b733d6423e1fc604cefad
James <healyjames97@gmail.com>
2023-11-23 22:01:06 +0000

    chore: update component to be more generic

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -3,11 +3,20 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledLayout = styled.div`
-    padding: ${props => props.theme.core.padding}rem ${props => (props.theme.core.padding * 2).toFixed(2)}rem ${props => (props.theme.core.padding * 2.5).toFixed(2)}rem ${props => (props.theme.core.padding * 2).toFixed(2)}rem;
-    margin: auto 0;
+    max-width: ${(props) => (props.theme.breakpoints.md - 25)}px;
+    margin: ${(props) => (props.theme.core.margin * 3)}rem auto;
+    padding: ${(props) => (props.theme.core.padding / 2)}rem ${(props) => (props.theme.core.padding * 2)}rem ${(props) => (props.theme.core.padding * 2)}rem ${(props) => (props.theme.core.padding * 2)}rem;
+
+    @media (min-width: ${props => props.theme.breakpoints.md}px) {
+        max-width: ${(props) => (props.theme.breakpoints.md - 50)}px;
+    }
 
     @media (min-width: ${props => props.theme.breakpoints.lg}px) {
-        padding: ${props => (props.theme.core.padding * 6).toFixed(2)}rem;
+        max-width: ${(props) => (props.theme.breakpoints.lg - 100)}px;
+    }
+
+    @media (min-width: ${props => props.theme.breakpoints.xl}px) {
+        max-width: ${(props) => (props.theme.breakpoints.xl - 150)}px;
     }
 `
 
